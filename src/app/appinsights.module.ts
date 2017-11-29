e61f2279daec7f5615a20f3000d8cdd83067c8cf
Manish Ramchand <maniram@microsoft.com>
2017-11-29 10:43:02 +0530

    Ng packager used for packaging

---
+++ src/app/appinsights.module.ts
@@ -0,0 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppInsightsService } from './appinsights.service';
+
+@NgModule({
+  declarations: [],
+  imports: [BrowserModule],
+  exports: [],
+  providers: [AppInsightsService],
+  bootstrap: []
+})
+export class AppInsightsModule { }
--- src/app/appinsights.service.spec.ts
+++ src/app/appinsights.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { AppInsightsService } from './logger.service';
+import { AppInsightsService } from './appinsights.service';
 
 describe('AppInsightsService', () => {
   beforeEach(() => {
